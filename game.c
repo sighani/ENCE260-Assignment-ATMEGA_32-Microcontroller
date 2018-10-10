c95bbdf53707e965be74cd7c37c6b53528e83c31
Mary Hongthongkhan <sho101@uclive.ac.nz>
2018-10-10 16:00:28 +1300

    Update game.c

--- game.c
+++ game.c
@@ -1,22 +1,81 @@
 #include "system.h"
+#include "pacer.h"
+#include "navswitch.h"
+#include "ir_uart.h"
+#include "tinygl.h"
+#include "../fonts/font5x7_1.h"
+
+#define PACER_RATE 2000
+#define MESSAGE_RATE 10
+
+void display_character (char character)
+{
+    char buffer[2];
+    buffer[0] = character;
+    buffer[1] = '\0';
+    tinygl_text (buffer);
+}
+
 
 int main (void)
 {
     system_init ();
-<<<<<<< HEAD
-    system_init ();
-    led_int();
     ir_uart_init ();
+    tinygl_init (PACER_RATE);
+    tinygl_font_set (&font5x7_1);
+    tinygl_text_speed_set (MESSAGE_RATE);
+    navswitch_init ();
 
-
-=======
-    printf("HLEOMEFMF");
->>>>>>> e3fd81c985a9fb577682dfc3c91bd5587a1b22e3
+    /* TODO: Initialise IR driver.  */
+    tinygl_text("START!\0");
 
     while (1)
     {
-
-
-
-    }
+        int count =0;
+        pacer_wait ();
+        tinygl_update ();
+        navswitch_update();
+        /* TODO: Call the navswitch update function.  */
+        if (navswitch_push_event_p(NAVSWITCH_NORTH)) {
+            display_character ('R');
+            ir_uart_putc('R');
+            count++;
+            if (ir_uart_read_ready_p()) {
+                if (ir_uart_getc() == 'R') {
+                    display_character('0');
+                } else if (ir_uart_getc() == 'S') {
+                    display_character('W');
+                } else if (ir_uart_getc() == 'P') {
+                    display_character('L');
+                }
+            }
+        } else if (navswitch_push_event_p(NAVSWITCH_SOUTH) && count == 1) {
+            display_character ('P');
+            ir_uart_putc('P');
+            count++;
+            if (ir_uart_read_ready_p()) {
+                if (ir_uart_getc() == 'P') {
+                    display_character('0');
+                } else if (ir_uart_getc() == 'S') {
+                    display_character('L');
+                } else if (ir_uart_getc() == 'R') {
+                    display_character('W');
+                }
+            }
+        } else if (navswitch_push_event_p(NAVSWITCH_WEST) && count == 2) {
+            display_character ('S');
+            ir_uart_putc('S');
+            count++;
+            if (ir_uart_read_ready_p()) {
+                if (ir_uart_getc() == 'S') {
+                    display_character('0');
+                } else if (ir_uart_getc() == 'P') {
+                    display_character('W');
+                } else if (ir_uart_getc() == 'R') {
+                    display_character('L');
+                    }
+                    }
+                    }
+                }
+        return 0;
 }
