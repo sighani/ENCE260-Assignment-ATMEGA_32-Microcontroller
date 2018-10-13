b7909777bdf84d8eecc67b227107f94be0450953
Saman Ighani <sig16@uclive.ac.nz>
2018-10-13 02:58:23 -0700

    Updated game.c everything is split into different helper function and then called in main and have global variables up the top

--- game.c
+++ game.c
@@ -11,6 +11,17 @@
 #define PACER_RATE 1000
 #define MESSAGE_RATE 25
 
+static int p1_is_ready = 0;
+static int p2_is_ready = 0;
+static char p2_choice;
+static char result;
+static char p2_choice;
+static int choice = 1;
+static const char item_select[3] =
+{
+	'R', 'P', 'S'
+};
+
 //display function
 void display_character (char character)
 {
@@ -31,18 +42,12 @@ void welcome (void)
 static char select_item(void)
 {
     int i = 0;
-    int choice = 1;
-    int p1_is_ready = 0;
-    int p2_is_ready = 0;
-    char result;
-    char p2_choice;
-    char item_select[3] = {'R', 'P', 'S'};
+
 
     while (1) {
-        //select from P, R, S
-        pacer_wait ();
-        tinygl_update ();
-        navswitch_update ();
+		pacer_wait ();
+		tinygl_update ();
+        navswitch_update ();        
         if (p1_is_ready == 0) {
             if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
                 choice++;
@@ -50,77 +55,85 @@ static char select_item(void)
                 if (choice > 2) {
                     choice = 0;
                 }
-            }
-            if (navswitch_push_event_p (NAVSWITCH_SOUTH)) {
+            } else if (navswitch_push_event_p (NAVSWITCH_SOUTH)) {
                 choice--;
                 i=1;
                 if (choice <0) {
                     choice = 2;
                 }
-            }
-            if (navswitch_push_event_p (NAVSWITCH_EAST)) {
+            } else if (navswitch_push_event_p (NAVSWITCH_EAST)) {
                 choice++;
                 i=1;
                 if (choice > 2) {
                     choice = 0;
                 }
-            }
-            if (navswitch_push_event_p (NAVSWITCH_WEST)) {
+            } else if (navswitch_push_event_p (NAVSWITCH_WEST)) {
                 choice--;
                 i=1;
                 if (choice <0) {
                     choice = 2;
                 }
-            }
-            if(i==1) {
+            } else if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
+				ir_uart_putc(item_select[choice]);
+				p1_is_ready = 1;
+				led_on ();
+				
+			}
+			if(i == 1) {
                 display_character(item_select[choice]);
             }
-
         }
-        //make the choice
-        if (navswitch_push_event_p (NAVSWITCH_PUSH)&&i != 0) {
-            ir_uart_putc(item_select[choice]);
-            p1_is_ready = 1;
-            led_on ();
-        }
-        //receive from p2
-        if (ir_uart_read_ready_p ()) {
-            p2_choice = ir_uart_getc ();
-            if(p2_choice != '\0') {
-                p2_is_ready = 1;
-            }
-        }
-        //compare the tow choice and display the result
-        if (p1_is_ready == 1 && p2_is_ready == 1) {
-            if(item_select[choice] == p2_choice) {
-                result = 'D';
-            } else if (item_select[choice] == 'P' && p2_choice == 'S') {
-                result = 'L';
-            } else if (item_select[choice] == 'S' && p2_choice == 'R') {
-                result = 'L';
-            } else if (item_select[choice] == 'R' && p2_choice == 'P') {
-                result = 'L';
-            } else if (item_select[choice] == 'S' && p2_choice == 'P') {
-                result = 'W';
-            } else if (item_select[choice] == 'R' && p2_choice == 'S') {
-                result = 'W';
-            } else if (item_select[choice] == 'P' && p2_choice == 'R') {
-                result = 'W';
-            }
+	}
+	return item_select[choice];
+}
 
-            //display the result
-            display_character(result);
-        }
-
-
-        if (button_pressed_p()) {
-            p1_is_ready = 0;
-            p2_is_ready = 0;
-            choice = 0;
-            led_off();
+static char get_player2_state (void)
+{
+    //receive from p2
+    if (ir_uart_read_ready_p ()) {
+        p2_choice = ir_uart_getc ();
+        if(p2_choice != '\0') {
+            p2_is_ready = 1;
         }
     }
+    return p2_choice;
 }
+
+void end_result (char player1_state, char p2_choice)
+{
+    //compare the tow choice and display the result
+    if (p1_is_ready == 1 && p2_is_ready == 1) {
+        if(player1_state == p2_choice) {
+            result = 'D';
+        } else if (player1_state == 'P' && p2_choice == 'S') {
+            result = 'L';
+        } else if (player1_state == 'S' && p2_choice == 'R') {
+            result = 'L';
+        } else if (player1_state == 'R' && p2_choice == 'P') {
+            result = 'L';
+        } else if (player1_state == 'S' && p2_choice == 'P') {
+            result = 'W';
+        } else if (player1_state == 'R' && p2_choice == 'S') {
+            result = 'W';
+        } else if (player1_state == 'P' && p2_choice == 'R') {
+            result = 'W';
+        }
+
+        //display the result
+        display_character(result);
+    }
+}
+
+static void game_reset (void)
+{
+    if (button_pressed_p()) {
+        p1_is_ready = 0;
+        p2_is_ready = 0;
+        choice = 0;
+        led_off();
+    }
+}
+
 
 
 //main function for the game
@@ -141,7 +154,13 @@ int main (void)
     welcome();
 
     //while loop for the game
-    select_item();
+    while (1) {
 
-
+        char player1_state = '\0';
+        char player2_state = '\0';
+        player1_state = select_item();
+        player2_state = get_player2_state();
+        end_result(player1_state, player2_state);
+        game_reset();
+    }
 }
